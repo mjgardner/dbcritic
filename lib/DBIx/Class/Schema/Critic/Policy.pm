f1cff162d569009ac9cca54a033f59000ae98207
Mark Gardner <mjgardner@cpan.org>
2011-07-18 01:06:25 -0400

    add NoPrimaryKey policy

--- lib/DBIx/Class/Schema/Critic/Policy.pm
+++ lib/DBIx/Class/Schema/Critic/Policy.pm
@@ -7,6 +7,7 @@ use Modern::Perl;
 use English '-no_match_vars';
 use Moose::Role;
 use MooseX::Has::Sugar;
+use MooseX::Types::Moose 'ArrayRef';
 use MooseX::Types::DBIx::Class 'Schema';
 use DBIx::Class::Schema::Critic::Types 'DBICType';
 use namespace::autoclean;
@@ -21,7 +22,20 @@ Returns a short string describing what's wrong.
 
 Returns a string giving further details.
 
+=head2 applies_to
+
+Returns an array reference of L<TypeConstraint|Moose::Meta::TypeConstraint>s
+s indicating what part(s) of the schema the policy is interested in.  Select
+from the list defined in
+L<DBICType|DBIx::Class::Schema::Critic::Types/DBICType>.
+
 =head2 violates
+
+Role consumers must implement a C<violates> method that returns true if the
+policy is violated and false otherwise, based on attributes provided by the
+role.  Callers should call the C<violates> method as the following:
+
+    $policy->violates($element, $schema);
 
 =over
 
@@ -35,7 +49,15 @@ object if it doesn't.
 
 =cut
 
-requires qw(description explanation violates);
+requires qw(description explanation applies_to violates);
+
+around violates => sub {
+    my ( $orig, $self ) = splice @ARG, 0, 2;
+    $self->_set_element(shift);
+    $self->_set_schema(shift);
+    return $self->violation if $self->$orig(@ARG);
+    return;
+};
 
 =attr element
 
@@ -58,27 +80,23 @@ L<DBIx::Class::Schema::Critic|DBIx::Class::Schema::Critic>.
 
 =cut
 
-has schema => ( ro, isa => Schema );
+has schema => ( ro, isa => Schema, writer => '_set_schema' );
 
-=method violation
+=attr violation
 
-=over
-
-=item Arguments: none
-
-=item Return value: A new
+Read-only accessor for a
 L<DBIx::Class::Schema::Critic::Violation|DBIx::Class::Schema::Critic::Violation>
 object based on the state of the current policy.
 
-=back
-
 =cut
 
-sub violation {
-    my $self = shift;
-    return DBIx::Class::Schema::Critic::Violation->new(
-        map { $ARG => $self->$ARG } qw(description explanation element),
-    );
-}
+has violation => ( ro, lazy,
+    init_arg => undef,
+    default  => sub {
+        DBIx::Class::Schema::Critic::Violation->new(
+            map { $ARG => $ARG[0]->$ARG }
+                qw(description explanation element) );
+    },
+);
 
 1;
---
+++ lib/DBIx/Class/Schema/Critic/Policy/NoPrimaryKey.pm
@@ -0,0 +1,42 @@
+package DBIx::Class::Schema::Critic::Policy::NoPrimaryKey;
+
+# ABSTRACT: Check for DBIx::Class::Schema::ResultSources without primary keys
+
+use utf8;
+use Modern::Perl;
+use Const::Fast;
+use English '-no_match_vars';
+use Moose;
+use MooseX::Has::Sugar;
+use MooseX::Types::DBIx::Class 'ResultSource';
+use MooseX::Types::Moose qw(ArrayRef Str);
+use namespace::autoclean;
+
+const my %ATTR => (
+    description => 'No primary key for a ResultSource',
+    explanation => <<'END_EXPLANATION',
+ResultSource tables should have one or more columns defined as a primary key.
+END_EXPLANATION
+);
+
+while ( my ( $name, $default ) = each %ATTR ) {
+    has $name => ( ro, isa => Str, default => $default );
+}
+has applies_to => ( ro,
+    isa     => 'ArrayRef[Moose::Meta::TypeConstraint]',
+    default => sub { [ResultSource] },
+);
+
+with 'DBIx::Class::Schema::Critic::Policy';
+
+=method violates
+
+This policy returns a violation if a
+L<DBIx::Class::ResultSource|DBIx::Class::ResultSource> has zero primary columns.
+
+=cut
+
+sub violates { return !scalar $ARG[0]->element->primary_columns }
+
+__PACKAGE__->meta->make_immutable();
+1;
